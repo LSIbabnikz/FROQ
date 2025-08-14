82284b80c9c6a5e8d8bc7c471cad55a895a6b69a
LSIbabnikz <ziga.babnik@fe.uni-lj.si>
2025-08-14 17:05:02 +0200

    Removing testing fluff and personal paths

--- run.sh
+++ run.sh
@@ -5,5 +5,3 @@ python3 ./baselineFIQA/main.py  -c ./baselineFIQA/baseline_FIQA_config.yaml
 python3 ./observer/initialization.py -c ./observer/initialization_config.yaml
 
 python3 ./observer/inference.py -c ./observer/inference_config.yaml
-
-python3 delete_me.py
